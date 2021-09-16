60ab21b0326aa1e932ec440b5b0260a8cfb653a7
reflectionprogramme <reflection.programme@gmail.com>
2021-09-16 23:03:24 +0700

    Fixing header, crew and cast tabs sections, adding logo

--- app/js/script.js
+++ app/js/script.js
@@ -1,22 +1,11 @@
-const hamburgerBtn = document.querySelector(".hamburger__button");
-const header = document.querySelector(".header");
-const mobile = document.querySelector(".mobile");
-
-hamburgerBtn.addEventListener("click", (e) => {
-  e.preventDefault();
-  console.log("hamburger button clicked");
-  if (
-    header.classList.contains("open") &&
-    mobile.classList.contains("expand")
-  ) {
-    header.classList.remove("open");
-    mobile.classList.remove("expand");
-  } else {
-    header.classList.add("open");
-    mobile.classList.add("expand");
-  }
+$(document).ready(function () {
+  $(".nav__hamburger").click(function (e) {
+    e.preventDefault();
+    $(".nav").toggleClass("open");
+    if ($(".nav").hasClass("open")) {
+      $(".mobile").slideDown();
+    } else {
+      $(".mobile").slideUp();
+    }
+  });
 });
-
-const year = document.getElementById("year");
-const date = new Date();
-year.innerText = date.getFullYear();
